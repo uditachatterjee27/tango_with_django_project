e913aec240e36e7da67eaf76998a04a59a9d0169
Udita Chatterjee <2761899c@student.gla.ac.uk>
2024-02-16 04:26:48 +0000

    chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from rango import views
 app_name = 'rango'
 urlpatterns = [
-path('', views.index, name='index'),
-path('about/', views.about, name='about'),
+    path('', views.index,name='index'),
+    path('about/', views.about, name='about'),
 ]
